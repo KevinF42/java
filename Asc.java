bfdc855a532850d636ad206ea90e07ee9d2e45aa
KevinF42 <kforrister@segfault.com>
2016-09-30 08:25:35 -0700

    Add files via upload

---
+++ Asc.java
@@ -0,0 +1,12 @@
+class Asc {
+	public static void main (String a[]){
+		//variables
+		int i;// interger i
+		char c;// character c
+		//for loop
+		for (i = 0; i < 8; i++){
+			c = (char)i;
+			System.out.print(i+"="+c+" ");
+		}
+	}
+}
