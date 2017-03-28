a9cbe9db1a5ae1715e6b868d3e3a35db83054775
KevinForrister <kforrister@segfault.com>
2017-03-28 10:03:45 -0700

    added all

--- Code2/trigtabledegreetime.java
+++ Code2/trigtabledegreetime.java
@@ -18,7 +18,7 @@ class TrigTableDegTime {
 		System.out.println("----------------------------------------------------------------------" );
 		// 9 223 372 036 854 775 807   18446744073709551614
 		// compile then change 2147483648 to 2147483647
-		while (d <=  2147483647){
+		while (d <=  360){
 			//convert angle t to radians r
 			r = d * (pi/180.0);
 			c = Math.cos(r);
@@ -30,9 +30,9 @@ class TrigTableDegTime {
 			rround = (Math.round(r*1000.0))/1000.0;
 
 			if (c == 0.0)
-				{// System.out.println("\t"+d+ "\t|\t"+ c +"\t|\t"+ s + "\t|\t UN ");  
+				{System.out.println("\t"+d+ "\t|\t"+ c +"\t|\t"+ s + "\t|\t UN ");  
 				}else{
-				 // System.out.println("\t"+d+ "\t|\t"+ c +"\t|\t"+ s + "\t|\t" + t);
+				  System.out.println("\t"+d+ "\t|\t"+ c +"\t|\t"+ s + "\t|\t" + t);
 				}
 			d = d + i;		
 		}
@@ -45,11 +45,11 @@ class TrigTableDegTime {
 			System.out.println(ms0);
 			System.out.print("AFTER LOOP\t\t\t");
 			System.out.println(ms1);
-		//	System.out.format("%.10f", ms0);
+			System.out.format("%.10f", ms0);
 			System.out.println();
-		//	System.out.format("%.10f", ms1);
+			System.out.format("%.10f", ms1);
 				System.out.println("********************************");
-		// end of time calculation
+		 //end of time calculation
 		
 	}
 }
